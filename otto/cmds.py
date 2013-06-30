06cb96622eefda8eda939e9d0f014952230e7ac1
Nic Roland <nicroland9@gmail.com>
2013-06-30 13:36:47 +0100

    Added open_config() and save_config() to otto.utils + some docstrings too.

--- otto/cmds.py
+++ otto/cmds.py
@@ -1,7 +1,6 @@
-from otto.utils import OttoCmd, OTTO_DIR, OTTO_CONFIG
+from otto.utils import *
 import os
 import os.path
-import json
 
 class New(OttoCmd):
     cmd_template = """import otto.utils as otto
@@ -19,28 +18,14 @@ class %s(otto.OttoCmd):
             cmd_args = args[1:]
             path = cmd_name + '.py'
 
-            # Ensure .otto/ exists
-            if not os.path.isdir(OTTO_DIR):
-                os.mkdir(OTTO_DIR)
-
-            # Try opening current config
-            config = {}
-            if os.path.isfile(OTTO_CONFIG):
-                with open(OTTO_CONFIG, 'r') as j:
-                    config = json.load(j)
+            config = open_config()
 
             # Add local_cmds section to config
             if 'local_cmds' not in config:
                 config['local_cmds'] = {}
             config['local_cmds'][cmd_name] = path
 
-            # TODO: Add section with kwargs for cmd to config
-
-            # Save
-            with open(OTTO_CONFIG, 'w') as j:
-                json.dump(config, j, indent=4)
-
-            # TODO: Create arg_list for cmd_template
+            save_config(config)
 
             with open(os.path.join(OTTO_DIR, path), 'w') as cmd_file:
                 cmd_file.write(
--- otto/utils.py
+++ otto/utils.py
@@ -2,11 +2,13 @@ import abc
 import os
 import os.path
 import sys
+import json
 
 OTTO_DIR = '.otto'
 OTTO_CONFIG = os.path.join(OTTO_DIR, 'config.json')
 
 class OttoCmd(object):
+    """Base class for all `otto` commands"""
     __metaclass__ = abc.ABCMeta
 
     @classmethod
@@ -19,5 +21,31 @@ class OttoCmd(object):
         return
 
     def cmd_usage(self, args):
+        """Print useage for this command and exit"""
         print "Usage:\n  $ otto %s %s" % (self._name(), ' '.join(args))
         sys.exit()
+
+def open_config():
+    """Opens config file, creates .otto/ folder if needed"""
+    _ensure_otto_dir()
+
+    # Try opening current config
+    config = {}
+    if os.path.isfile(OTTO_CONFIG):
+        with open(OTTO_CONFIG, 'r') as j:
+            config = json.load(j)
+
+    return config
+
+def save_config(config):
+    """Save config to ./.otto/config.json"""
+    _ensure_otto_dir()
+
+    # Save
+    with open(OTTO_CONFIG, 'w') as j:
+        json.dump(config, j, indent=4)
+
+def _ensure_otto_dir():
+    # Ensure .otto/ exists
+    if not os.path.isdir(OTTO_DIR):
+        os.mkdir(OTTO_DIR)
