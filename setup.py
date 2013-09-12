f8f12a1ec627379e3b37c3d90c9ada605f965e33
Nic Roland <nicroland9@gmail.com>
2013-09-12 21:27:11 +0000

    Additional work on the post-job tone option.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author='Nic Roland',
     author_email='nicroland9@gmail.com',
     packages=['otto'],
+    data_files=[('otto_res', ['res/config.json', 'res/done.wav'])],
     scripts=['bin/otto'],
-    data_files=[('otto_res', ['res/config.json'])],
     description='For automating your workflow',
     long_description=open('README.rst').read(),
 )
