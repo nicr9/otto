3027efc87b4a344573707a42438007046f62c019
Nic Roland <nicroland9@gmail.com>
2013-11-18 09:03:10 +0000

    Rewrote install + more changes to path constants.

--- otto/__init__.py
+++ otto/__init__.py
@@ -2,8 +2,8 @@ import os
 import os.path
 
 # Global paths
-GLOBAL_CMDS_DIR = os.path.expanduser('~/.otto')
-GLOBAL_CONFIG = os.path.join(GLOBAL_CMDS_DIR, 'config.json')
+GLOBAL_DIR = os.path.expanduser('~/.otto')
+GLOBAL_CONFIG = os.path.join(GLOBAL_DIR, 'config.json')
 
 # Local paths
 LOCAL_DIR = os.path.join(os.getcwd(), '.otto')
--- otto/cmds.py
+++ otto/cmds.py
@@ -127,21 +127,32 @@ class Install(OttoCmd):
     """Install a package as global commands"""
 
     def run(self, pack_path):
+        from shutil import copytree, rmtree
         pack_name = os.path.basename(pack_path).split('.opack')[0]
-
-        # Open global config
-        config = open_config(GLOBAL_CONFIG)
-
-        # Add pack
-        if 'packs' not in config:
-            config['packs'] = []
-        config['packs'].append(pack_name)
-
-        # Save global config
-        save_config(config, GLOBAL_CONFIG)
+        temp_dir = os.path.join(GLOBAL_DIR, '_installing') 
 
         # Untar pack
-        shell(r'tar -xzf %s -C %s' % (pack_path, GLOBAL_CMDS_DIR))
+        shell(r'tar -xzf %s -C %s' % (
+            pack_path, 
+            temp_dir,
+            ))
+
+        # Copy pack info to global config
+        with ConfigFile(os.path.join(temp_dir, 'config.json')) as pack_config:
+            to_install = pack_config['packs']
+
+        with ConfigFile(GLOBAL_CONFIG, True) as installed_config:
+            packs = installed_config.get('packs', {})
+            packs.update(to_install)
+
+        # Copy pack cmds to GLOBAL_DIR
+        copytree(
+                os.path.join(temp_dir, pack_name),
+                os.path.join(GLOBAL_DIR, pack_name),
+                )
+
+        # Clean up
+        rmtree(temp_dir)
 
 class Uninstall(OttoCmd):
     """Interactive prompt to remove installed package"""
@@ -163,7 +174,7 @@ class Uninstall(OttoCmd):
         # Remove pack
         config['packs'].pop(dialog.index)
         save_config(config, GLOBAL_CONFIG)
-        rmtree(os.path.join(GLOBAL_CMDS_DIR, dialog.result))
+        rmtree(os.path.join(GLOBAL_DIR, dialog.result))
 
 class gitcheck(OttoCmd):
     """Make sure no '# TODO:'s were left behind before commiting"""
