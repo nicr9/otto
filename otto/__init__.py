7c89b0ead99b369a223376afce69cfff74bfd291
Nic Roland <nicroland9@gmail.com>
2014-12-28 14:18:04 +0000

    Bumped version to v0.7.

--- otto/__init__.py
+++ otto/__init__.py
@@ -1,7 +1,12 @@
 import os
 import os.path
 
-# Config file names
+# Misc constants
+OTTO_VERSION = 'v0.7'
+OTTO_DESC = "A workflow automator written in Python."
+
+# File names and extensions
+PACK_EXT = '.opack'
 ROOT_FILE = 'config.json'
 CMDS_FILE = 'cmds.json'
 
@@ -13,9 +18,3 @@ GLOBAL_CONFIG = os.path.join(GLOBAL_DIR, ROOT_FILE)
 LOCAL_DIR = os.path.join(os.getcwd(), '.otto')
 LOCAL_CONFIG = os.path.join(LOCAL_DIR, ROOT_FILE)
 LOCAL_CMDS_DIR = os.path.join(LOCAL_DIR, 'local')
-
-# Other constants
-PACK_EXT = '.opack'
-OTTO_VERSION = 'v0.7a'
-OTTO_DESC = "A workflow automator written in Python."
-
