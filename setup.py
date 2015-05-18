3403ad11853121e17e242d3381604268604386cf
Nic Roland <nicroland9@gmail.com>
2015-05-18 22:48:42 +0100

    Updated setup.py in prep for push to PYPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from otto import OTTO_VERSION, OTTO_DESC
 
 setup(
-    name='otto',
+    name='otto_base',
     version=OTTO_VERSION,
     author='Nic Roland',
     author_email='nicroland9@gmail.com',
@@ -13,6 +13,20 @@ setup(
     description=OTTO_DESC,
     install_requires=[
         'lament',
-        'pydialog'
-        ,
+        'pydialog',
+        ],
+    license='MIT',
+    url = 'https://github.com/nicr9/otto',
+    download_url = 'https://github.com/nicr9/otto/tarball/%s' % OTTO_VERSION,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+        ],
 )
